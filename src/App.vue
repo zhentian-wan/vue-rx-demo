de37452a4870377a0623551cb20ff01ee7e34fa8
Zhentian Wan <answer881215@gmail.com>
2018-07-16 22:50:37 +0300

    get started with vue-rx

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>{{interval$}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {interval, of} from 'rxjs'
+import {switchMap} from 'rxjs/operators'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  subscriptions: {
+    interval$: interval(1000)
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import 'buefy/lib/buefy.css'
+
 import Vue from 'vue'
 import App from './App.vue'
+
+import Rx from 'rxjs'
+import VueRx from 'vue-rx'
+
+import Buefy from 'buefy'
+
+Vue.use(VueRx, Rx)
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 

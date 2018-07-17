e60e776ebeadecc59fcf5c89e29ebe595ad606cb
zwan <zhentian.wan@ssh.com>
2018-07-17 15:22:18 +0300

    using axios to load data from server

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <section class="section">
     <button class="button" v-stream:click="click$">Click</button>
-    <h1 class="title">{{random$}}</h1>
+    <h2>
+      {{luke$}}
+    </h2>
   </section>
 </template>
 
 <script>
-import { interval, of } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { switchMap, pluck } from 'rxjs/operators';
 
 export default {
   name: 'app',
   domStreams: ['click$'],
   subscriptions() {
 
-    const random$ = this.click$.pipe(
-      map(() => Math.random())
+    const people$ = from(
+      this.$http.get(
+        "https://starwars.egghead.training/people/1"
+      )
+    )
+
+    const luke$ = this.click$.pipe(
+      switchMap(() => people$),
+      pluck('data', 'name')
     )
 
     return {
-      random$
+      luke$
     }
   }
 };

ef2362a8dabc9a3edf559521548e6ad138fa690e
zwan <zhentian.wan@ssh.com>
2018-07-18 13:52:24 +0300

    share the request

--- src/App.vue
+++ src/App.vue
@@ -9,39 +9,33 @@
 </template>
 
 <script>
-import { from } from 'rxjs';
-import { switchMap, pluck, map } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { switchMap, pluck, map, mapTo, catchError, shareReplay, share } from 'rxjs/operators';
 
 export default {
   name: 'app',
   domStreams: ['click$'],
   subscriptions() {
-
-    const person$ = from(
-      this.$http.get(
-        "https://starwars.egghead.training/people/1"
-      )
-    ).pipe(
-      pluck('data')
-    )
+    const createLoader = url => from(this.$http.get(url)).pipe(pluck('data'));
 
     const luke$ = this.click$.pipe(
-      switchMap(() => person$)
-    )
+      mapTo('https://starwars.egghead.trainin/people/1'),
+      switchMap(createLoader),
+      catchError(() => createLoader('https://starwars.egghead.training/people/2')),
+      share()
+    );
 
-    const name$ = luke$.pipe(
-      pluck('name')
-    )
+    const name$ = luke$.pipe(pluck('name'));
 
     const image$ = luke$.pipe(
       pluck('image'),
-      map(src => `https://starwars.egghead.training/${src}` )
-    )
+      map(src => `https://starwars.egghead.training/${src}`)
+    );
 
     return {
       name$,
       image$
-    }
+    };
   }
 };
 </script>

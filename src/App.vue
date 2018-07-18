441ee5842de1dd96a8357fb33ddb5e4f71dd3dc9
zwan <zhentian.wan@ssh.com>
2018-07-18 14:32:03 +0300

    handle disable

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,71 @@
 <template>
   <section class="section">
-    <button class="button" v-stream:click="click$">Click</button>
+    <button class="button" :disabled="disabled$" v-stream:click="click$">{{buttonText$}}</button>
     <h2>
       {{name$}}
     </h2>
-    <img :src="image$" alt="">
+    <img v-stream:error="imageError$" :src="image$" alt="">
   </section>
 </template>
 
 <script>
-import { from, of } from 'rxjs';
-import { switchMap, pluck, map, mapTo, catchError, shareReplay, share } from 'rxjs/operators';
+import { from, of, merge, throwError } from 'rxjs';
+import {
+  switchMap,
+  pluck,
+  map,
+  mapTo,
+  catchError,
+  shareReplay,
+  share,
+  startWith,
+  concat,
+withLatestFrom
+} from 'rxjs/operators';
 
 export default {
   name: 'app',
-  domStreams: ['click$'],
+  domStreams: ['click$', 'imageError$'],
   subscriptions() {
     const createLoader = url => from(this.$http.get(url)).pipe(pluck('data'));
 
     const luke$ = this.click$.pipe(
       mapTo('https://starwars.egghead.trainin/people/1'),
       switchMap(createLoader),
-      catchError(() => createLoader('https://starwars.egghead.training/people/2')),
+      catchError(() => of({name: 'Failed.. :('})),
       share()
     );
 
     const name$ = luke$.pipe(pluck('name'));
 
-    const image$ = luke$.pipe(
+    const loadImage$ = luke$.pipe(
       pluck('image'),
       map(src => `https://starwars.egghead.training/${src}`)
     );
 
+    const failImage$ = this.imageError$.pipe(mapTo(
+      `http://via.placeholder.com/400x400`
+    ));
+
+    const image$ = merge(
+      loadImage$,
+      failImage$
+    )
+
+    const disabled$ = withLatestFrom(
+      this.click$.pipe(mapTo(true)),
+      luke$.pipe(mapTo(false)),
+    ).pipe(startWith(false));
+
+    const buttonText$ = disabled$.pipe(map(b => (b ? 'Loading...' : 'Load')));
+
+
     return {
       name$,
-      image$
+      image$,
+      failImage$,
+      disabled$,
+      buttonText$
     };
   }
 };

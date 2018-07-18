3a529adb78243b27022990a5c11cd53bb3a98e9d
zwan <zhentian.wan@ssh.com>
2018-07-18 15:11:18 +0300

    change button to activeTab

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
+
+
   <section class="section">
-    <button class="button" :disabled="disabled$" v-stream:click="{subject: click$, data: 1}">{{buttonText$}}</button>
+
+  {{activeTab$}}
+  <b-tabs v-model="activeTab">
+    <b-tab-item v-for="person of people" :key="person.id" :label="person.name"></b-tab-item>
+  </b-tabs>
+  <!--  <button class="button" :disabled="disabled$" v-stream:click="{subject: click$, data: 1}">{{buttonText$}}</button>-->
     <h2>
       {{name$}}
     </h2>
@@ -24,15 +31,30 @@ import {
 
 export default {
   name: 'app',
+  data() {
+    return {
+      activeTab: 0,
+      people: [
+        { name: 'Luke', id: 1 },
+        { name: 'Darth', id: 4 },
+        { name: 'Leia', id: 5 },
+        { name: 'Tion', id: 20 }
+      ]
+    };
+  },
   domStreams: ['click$', 'imageError$'],
   subscriptions() {
+    const activeTab$ = this.$watchAsObservable('activeTab', {
+      immediate: true
+    }).pipe(pluck('newValue'));
+
     const createLoader = url => from(this.$http.get(url)).pipe(pluck('data'));
 
-    const luke$ = this.click$.pipe(
-      pluck('data'),
+    const luke$ = activeTab$.pipe(
+      map(index => this.people[index].id),
       map(id => `https://starwars.egghead.training/people/${id}`),
       exhaustMap(createLoader),
-      catchError(() => of({name: 'Failed.. :('})),
+      catchError(() => of({ name: 'Failed.. :(' })),
       share()
     );
 
@@ -47,25 +69,22 @@ export default {
       mapTo(`http://via.placeholder.com/400x400`)
     );
 
-    const image$ = merge(
-      loadImage$,
-      failImage$
-    )
+    const image$ = merge(loadImage$, failImage$);
 
     const disabled$ = merge(
       this.click$.pipe(mapTo(true)),
-      luke$.pipe(mapTo(false)),
+      luke$.pipe(mapTo(false))
     ).pipe(startWith(false));
 
     const buttonText$ = disabled$.pipe(map(b => (b ? 'Loading...' : 'Load')));
-
 
     return {
       name$,
       image$,
       failImage$,
       disabled$,
-      buttonText$
+      buttonText$,
+      activeTab$
     };
   }
 };
